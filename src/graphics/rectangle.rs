d497d5b23696c7dad32724358e4ab2a3b353de83
Kepler-17c <florian.gilges@gmx.de>
2018-04-05 14:15:46 +0200

    Mistake in function name corrected

--- src/graphics/rectangle.rs
+++ src/graphics/rectangle.rs
@@ -1,15 +1,15 @@
 use stm32f7::lcd::{Layer, Framebuffer, Color};
-use graphics::{line, point::Point};
+use graphics::{point::Point};
 
 pub fn draw_rectangle<T: Framebuffer> (lcd: &mut Layer<T>, p0: &Point, p1: &Point, color: Color, fill: bool) {
     if fill {
         for x in p0.x..p1.x {
             for y in p0.y..p1.y {
-                lcd.print_pixel_color_at(x, y, color);
+                lcd.print_point_color_at(x, y, color);
             }
         }
     } else {
-        let points = [
+        let _points = [
             Point {
                 x: p0.x,
                 y: p0.y,
