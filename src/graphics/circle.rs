ba38fc47aa3d15023bf1ad13a528204470bd9b6d
Xandra <uadpx@student.kit.edu>
2018-04-06 11:31:20 +0200

    "filled circle availiable now."

--- src/graphics/circle.rs
+++ src/graphics/circle.rs
@@ -1,7 +1,7 @@
 use stm32f7::lcd::{Color, Framebuffer, Layer};
 use graphics::point::Point;
 
-pub fn drawcircle<T: Framebuffer>(lcd: &mut Layer<T>,m: &Point, r : i32, color: Color) {
+pub fn draw_circle<T: Framebuffer>(lcd: &mut Layer<T>,m: &Point, r : i32, color: Color) {
     let x0 = m.x as i32;
     let y0 = m.y as i32;
     let mut x: i32 = r-1;
@@ -22,6 +22,46 @@ pub fn drawcircle<T: Framebuffer>(lcd: &mut Layer<T>,m: &Point, r : i32, color: 
             print_point_at(lcd, x0 as i32 - y, y0 as i32 - x, color);
             print_point_at(lcd, x0 as i32 + y, y0 as i32 - x, color);
             print_point_at(lcd, x0 as i32 + x, y0 as i32 - y, color);
+
+            if err <= 0 {
+                y += 1;
+                err += dy;
+                dy += 2;
+            }
+
+            if err > 0 {
+                x -= 1;
+                dx += 2;
+                err += dx - 2*r;
+            }
+        }
+    }
+
+}
+
+pub fn draw_filled_circle<T: Framebuffer>(lcd: &mut Layer<T>,m: &Point, r : i32, color: Color) {
+    let x0 = m.x as i32;
+    let y0 = m.y as i32;
+    let mut x: i32 = r-1;
+    let mut y: i32 = 0;
+    let mut dx : i32 = 1;
+    let mut dy : i32 =1;
+    let mut err :i32 = dx - 2*r;
+
+    loop{
+        if x < y {
+            break;
+        } else {
+            for i in 0..x {
+            print_point_at(lcd, x0 as i32 + i, y0 as i32 + y, color);
+            print_point_at(lcd, x0 as i32 + y, y0 as i32 + i, color);
+            print_point_at(lcd, x0 as i32 - y, y0 as i32 + i, color);
+            print_point_at(lcd, x0 as i32 - i, y0 as i32 + y, color);
+            print_point_at(lcd, x0 as i32 - i, y0 as i32 - y, color);
+            print_point_at(lcd, x0 as i32 - y, y0 as i32 - i, color);
+            print_point_at(lcd, x0 as i32 + y, y0 as i32 - i, color);
+            print_point_at(lcd, x0 as i32 + i, y0 as i32 - y, color);
+            }
 
             if err <= 0 {
                 y += 1;
