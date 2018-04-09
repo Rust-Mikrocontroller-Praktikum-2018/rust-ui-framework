795d58fee3d26f643256a9f74a00e13ae8fa2b35
Kepler-17c <florian.gilges@gmx.de>
2018-04-09 11:59:29 +0200

    Field renaming suggest

--- src/graphics/model.rs
+++ src/graphics/model.rs
@@ -8,6 +8,7 @@ pub struct Model {
 
 pub struct CursorModel {
     pub first_contact: Option<Point>,
+    // outvoted 2 out of 3 against renaming to drag_contact
     pub second_contact: Option<Point>,
     pub last_contact: Option<Point>,
 }
